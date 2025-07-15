3e66a6de18d7b71b6f26d002fd3d77be845494c4
MerriemWeebster <daniyalkhan2000@gmail.com>
2025-07-15 21:05:50 +0400

    CoreScenesManager Singleton
    Static Instance of CoreScenesManager assigned at Start.
    Additional private routine for LoadingScene.
    Public AddCoreScene method to allow load requests (parallel loading).

--- Assets/GameData/Scripts/Core/CoreScenesManager.cs
+++ Assets/GameData/Scripts/Core/CoreScenesManager.cs
@@ -6,8 +6,17 @@ namespace CardGame.Core
 {
     public class CoreScenesManager : MonoBehaviour
     {
+        public static CoreScenesManager Instance { get; private set; } // Singleton to handle requests from RequestControllers.
+
         [Tooltip("Sequence of scenes to load. Each scene is added additively.")]
         public CoreScenes[] LoadingSequence;
+
+        /// <summary>
+        /// Begins a Coroutine which additively loads a CoreScene.
+        /// Requests are handled parallelly.
+        /// </summary>
+        /// <param name="coreScene"></param>
+        public void AddCoreScene(CoreScenes coreScene) => StartCoroutine(LoadCoreScene(coreScene));
 
         /// <summary>
         /// This MonoBehaviour will execute a Start Coroutine that sequentially itterates through
@@ -17,14 +26,24 @@ namespace CardGame.Core
         /// <returns></returns>
         private IEnumerator Start()
         {
+            Instance = this;
             foreach (CoreScenes coreScene in LoadingSequence)
-            {
-                int sceneIndex = (int)coreScene;
-                AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
-                operation.allowSceneActivation = true;
-                yield return operation;
-                Debug.Log($"[{nameof(CoreScenesManager)}] Activated scene {coreScene} (sceneIndex)");
-            }
+                yield return LoadCoreScene(coreScene);
+        }
+
+        /// <summary>
+        /// LoadCoreScene routine which creates the async operation to load the
+        /// specified CoreScene additively.
+        /// </summary>
+        /// <param name="coreScene"></param>
+        /// <returns></returns>
+        private IEnumerator LoadCoreScene(CoreScenes coreScene)
+        {
+            int sceneIndex = (int)coreScene;
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+            operation.allowSceneActivation = true;
+            yield return operation;
+            Debug.Log($"[{nameof(CoreScenesManager)}] Activated scene {coreScene} (sceneIndex)");
         }
     }
 }
