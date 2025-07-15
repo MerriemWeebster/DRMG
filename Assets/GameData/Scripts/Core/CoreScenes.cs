3a66cdde90f59a7398272884b485c9c004813f78
MerriemWeebster <daniyalkhan2000@gmail.com>
2025-07-15 20:50:32 +0400

    Added Summary to CoreScenes.cs
    Short summary added regarding the CoreScenes enum.

--- Assets/GameData/Scripts/Core/CoreScenes.cs
+++ Assets/GameData/Scripts/Core/CoreScenes.cs
@@ -1,5 +1,8 @@
 namespace CardGame.Core
 {
+    /// <summary>
+    /// An enum of all CoreScenes assigned to their respective scene index.
+    /// </summary>
     public enum CoreScenes
     {
         Core = 0,
